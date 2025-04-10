d3b1ced25fccf51b48b0e542611e52eba99b7b92
Navina Lang <81016364+langnavina97@users.noreply.github.com>
2025-04-10 14:43:23 +0700

    issue-258-fix

--- contracts/wrappers/algebra-v1.2/SwapVerificationLibraryAlgebraV2.sol
+++ contracts/wrappers/algebra-v1.2/SwapVerificationLibraryAlgebraV2.sol
@@ -248,8 +248,8 @@ library SwapVerificationLibraryAlgebraV2 {
       .allowedRatioDeviationBps();
     // Calculate the maximum allowed balance after swap based on the deviation
     // This ensures that most of the tokenIn has been sold, leaving only a small dust value
-    uint256 dustAllowance = (_balanceBeforeSwap *
-      (TOTAL_WEIGHT - allowedRatioDeviationBps)) / TOTAL_WEIGHT;
+    uint256 dustAllowance = (_balanceBeforeSwap * allowedRatioDeviationBps) /
+      TOTAL_WEIGHT;
 
     if (_balanceAfterSwap > dustAllowance) {
       revert ErrorLibrary.InvalidSwapAmount();
