94e1ca2c526eb6409a8634c03d8aabe824c853f1
Navina Lang <81016364+langnavina97@users.noreply.github.com>
2025-07-14 19:07:38 +0700

    issue 381 fix

--- contracts/wrappers/uniswapV3/UniswapV3SwapHandler.sol
+++ contracts/wrappers/uniswapV3/UniswapV3SwapHandler.sol
@@ -1,16 +1,17 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.17;
 
-import { ISwapRouter } from "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import { ISwapRouter02 } from "./ISwapRouter02.sol";
+
 import { TransferHelper } from "@uniswap/lib/contracts/libraries/TransferHelper.sol";
 import { IERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 
 contract UniswapV3SwapHandler {
   address WETH;
-  ISwapRouter router;
+  ISwapRouter02 router;
 
   constructor(address _router, address _weth) {
-    router = ISwapRouter(_router);
+    router = ISwapRouter02(_router);
     WETH = _weth;
   }
 
@@ -28,13 +29,12 @@ contract UniswapV3SwapHandler {
     );
     _safeApprove(tokenIn, address(router), amountIn);
 
-    ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+    ISwapRouter02.ExactInputSingleParams memory params = ISwapRouter02
       .ExactInputSingleParams({
         tokenIn: tokenIn,
         tokenOut: tokenOut,
         fee: poolFee,
         recipient: msg.sender,
-        deadline: block.timestamp,
         amountIn: amountIn,
         amountOutMinimum: 0,
         sqrtPriceLimitX96: 0
@@ -49,9 +49,12 @@ contract UniswapV3SwapHandler {
    * @param spender The address of the spender.
    * @param amount The amount to approve.
    */
-  function _safeApprove(address token, address spender, uint256 amount) internal {
+  function _safeApprove(
+    address token,
+    address spender,
+    uint256 amount
+  ) internal {
     try IERC20Upgradeable(token).approve(spender, 0) {} catch {}
     TransferHelper.safeApprove(token, spender, amount);
   }
-
 }
