e9aa637cb60918da13dfcef91f9865e06bd9ba6d
havoc19 <akarshmaurya19@gmail.com>
2025-07-30 16:33:58 +0530

    fix:flashloan fee adjustment for thena

--- contracts/front-end-helpers/PortfolioCalculations.sol
+++ contracts/front-end-helpers/PortfolioCalculations.sol
@@ -657,6 +657,15 @@ contract PortfolioCalculations is ExponentialNoError {
     return IThena(_pool).globalState().lastFee;
   }
 
+  function getFlashLoanFeeFromPool(
+    address _factory,
+    address _token0,
+    address _token1
+  ) external view returns (uint256) {
+    address poolAddress = IThena(_factory).poolByPair(_token0, _token1);
+    return IThena(poolAddress).globalState().lastFee;
+  }
+
   function getCollateralAmountToSell(
     address _user,
     address _controller,
@@ -730,7 +739,7 @@ contract PortfolioCalculations is ExponentialNoError {
     uint256 borrowBalance,
     uint256 totalCollateral
   ) internal pure returns (uint256 debtValue, uint256 percentageToRemove) {
-    uint256 feeAmount = (_debtRepayAmount * 10 ** 18 * feeUnit) / 10 ** 22;
+    uint256 feeAmount = (_debtRepayAmount * 10 ** 18 * feeUnit) / 10 ** 24;
     uint256 debtAmountWithFee = _debtRepayAmount + feeAmount;
     debtValue = (debtAmountWithFee * totalDebt * 10 ** 18) / borrowBalance;
     percentageToRemove = debtValue / totalCollateral;
